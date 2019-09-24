5a51f0845b18c78fb979a75d6a60de651f7afacc
JmKanmo <nebi25@naver.com>
2019-09-24 21:39:02 +0900

    회원가입 - jQuery적용

--- api_version1/user.py
+++ api_version1/user.py
@@ -1,29 +1,69 @@
 from flask import jsonify
 from . import api
 from flask import request
-from models import userModel
+from models import UserModel
 from models import db
 
 
 @api.route('/users', methods=['GET', 'POST'])
 def users():
     if request.method == 'POST':
-        username = request.form.get('username')
-        userid = request.form.get('userid')
-        password = request.form.get('password')
-        birthdate = request.form.get('birthdate')
+        data = request.get_json()
+        username = data.get('username')
+        userid = data.get('userid')
+        password = data.get('password')
+        birthdate = data.get('birthdate')
 
         if not (userid and username and password):
             return jsonify({'error': 'No arguments'}), 400
 
-        user = userModel()
+        user = UserModel()
         user.id = userid
         user.name = username
         user.birthdate = birthdate
         user.password = password
-        
+
         db.session.add(user)
         db.session.commit()
         return jsonify(), 201
 
-    return jsonify()
+    else:
+        users_info = UserModel.query.all()
+
+    return jsonify([user_info.serialize for user_info in users_info])
+
+
+@api.route('/users/<uid>', methods=['GET', 'PUT', 'DELETE'])
+def user_detail(uid):
+    if request.method == 'GET':
+        user = UserModel.query.filter(UserModel.id == uid).first()
+        return jsonify(user.serialize)
+
+    elif request.method == 'DELETE':
+        UserModel.query.delete(UserModel.id == uid)
+        return jsonify('deletion completed'), 204
+
+    else:
+        data = request.get_json()
+        username = data.get('username')
+        userid = data.get('userid')
+        password = data.get('password')
+        birthdate = data.get('birthdate')
+
+        updated_data = {}
+
+        if userid:
+            updated_data['userid'] = userid
+
+        if username:
+            updated_data['username'] = username
+
+        if password:
+            updated_data['password'] = password
+
+        if birthdate:
+            updated_data['birthdate'] = birthdate
+
+        UserModel.query.filter(uid == UserModel.id).update(updated_data)
+        users_info = UserModel.query.all()
+        return jsonify([user_info.serialize for user_info in users_info])
--- models.py
+++ models.py
@@ -3,9 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()  # db객체생성
 
 
-class userModel(db.Model):
+class UserModel(db.Model):
     __tablename__ = "user_DB"
     id = db.Column(db.String(30), primary_key=True)
     password = db.Column(db.String(64))
     birthdate = db.Column(db.Integer)
     name = db.Column(db.String(64))
+
+    @property
+    def serialize(self):
+        return{
+            'id': self.id,
+            'password': self.password,
+            'name': self.name,
+            'birthdate': self.birthdate
+        }
